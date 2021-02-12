7ce0ff391617b08c6491dda82e7cddb2f22f91f5
schatzen <56854068+schatzen@users.noreply.github.com>
2021-02-12 15:43:15 +0900

    # 1. 변수,상수,타입

---
+++ src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,10 @@
+const val MAX_SPEED: Int = 5000
+
+fun main(args: Array <String>) {
+
+    val playerName = "Astragon"
+    var experiencePoints = 5
+    experiencePoints+= 5
+    println(experiencePoints)
+    println(playerName)
+}
